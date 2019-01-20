26503e60c4a502d7ad4275a1205265d10b5d78a8
boombarashk <boombarashk@yandex.ru>
2019-01-20 21:56:47 +0300

    Updates

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-export const CENTER_COORDS = [59.9874597, 30.3036836];
-export const THROTTLE_TIME = 800;
-
-export const MAPCONTAINER_CLASSNAME = "App-map";
-export const POINTS_CLASSNAME = "App-itemTitle";
